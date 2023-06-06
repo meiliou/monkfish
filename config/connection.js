cd53a4aa36ec72201baf852ded5d1e7e1cfb4cce
meiliou <83768277+meiliou@users.noreply.github.com>
2023-06-06 14:06:05 +0400

    config file added

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequelize(DB_Name, DB_USER, DB_PW, {
+const sequelize = new Sequelize(process.env.DB_Name, process.env.DB_USER, process.env.DB_PW, {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
