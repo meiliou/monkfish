d2e5063cca31289b7775dba1c3a52fe1aaa59483
meiliou <83768277+meiliou@users.noreply.github.com>
2023-06-14 03:11:18 +0400

    top half of single restaurant page rendered, conditional links displayed

--- public/javascript/view-single-restaurant.js
+++ public/javascript/view-single-restaurant.js
@@ -1,21 +1,39 @@
 // Function to handle the image click event
-async function handleImageClick(event) {
-  event.preventDefault();
+// async function imageClickHandler(event) {
+//   event.preventDefault();
 
-  // Get the restaurant ID from the clicked image's data-restaurant-id attribute
-  const restaurantId = event.target.dataset.restaurantId;
-  
-  console.log('Clicked image:', event.target); // Log the clicked image element
-  console.log('Restaurant ID:', restaurantId);
+//   // Get the restaurant ID from the clicked image's data-restaurant-id attribute
+// const restaurantData = JSON.parse(event.target.getAttribute('data-restaurant'));
+// console.log('Restaurant Data:', restaurantData);
+// console.log('Attribute Value:', event.target.getAttribute('data-restaurant'));
 
-  // Redirect the user to the single-restaurant page using the restaurant ID
-  window.location.href = `/restaurants/${restaurantId}`;
-}
+// const restaurantId = event.target.getAttribute('data-restaurant');
+//   console.log('Restaurant ID:', restaurantId);
+//   console.log('Attribute Value:', event.target.getAttribute('data-restaurant'))
+//     // Trim the restaurant ID and convert it to a number
+//     const trimmedRestaurantId = restaurantId.trim();
+//     const parsedRestaurantId = parseInt(trimmedRestaurantId, 10);
+//     console.log('Parsed restaurant ID:', parsedRestaurantId);
+    
+//       // Check if the parsed restaurant ID is a valid number
+//   if (!isNaN(parsedRestaurantId)) {
+//     // Redirect the user to the single-restaurant page using the restaurant ID
+//     window.location.href = `/restaurant/${parsedRestaurantId}`;
+//   } else {
+//     console.error('Invalid restaurant ID:', restaurantId);
+//   }
+// };
+
+// //   // Redirect the user to the single-restaurant page using the restaurant ID
+//   console.log('Before redirection');
+//   window.location.href = `/restaurant/${restaurantId}`;
+//   console.log('After redirection');
+// };
 
 // Attach the event listener to the image elements
-const restaurantImages = document.querySelectorAll('.restaurant-image');
-console.log('Number of image elements:', restaurantImages.length);
-restaurantImages.forEach(image => {
-  image.addEventListener('click', handleImageClick);
-  console.log('Added event listener to image:', image);
-});
+// const restaurantImages = document.querySelectorAll('.restaurant-image');
+// console.log('Number of image elements:', restaurantImages.length);
+// restaurantImages.forEach(image => {
+//   image.addEventListener('click', imageClickHandler);
+//   console.log('Added event listener to image:', image);
+// });
