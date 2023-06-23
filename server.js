bd752e2a6c5ac673dde3617de885e04fd0a1d894
withchristopher <chmaxwel@icloud.com>
2023-06-23 18:19:38 +0200

    WIP 3

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const app = express();
 const hbs = exphbs.create({ helpers });
 const PORT = process.env.PORT || 3001;
 
-const signupRoutes = require('./controllers/api/user-routes');
-const loginRoutes = require('./controllers/api/user-routes');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
